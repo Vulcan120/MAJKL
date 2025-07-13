b9249116341b874c3dc97a018b21fa418d00efd6
Kenneth Oliver <kenneth.ksoiii@gmail.com>
2025-07-13 06:04:04 +0000

    Update: Phantom Wallet support for mobile (i.e., iOS Safari)

--- src/components/providers/wallet-provider.tsx
+++ src/components/providers/wallet-provider.tsx
@@ -3,13 +3,23 @@ import React, { type FC, useMemo, type ReactNode } from 'react';
 import { ConnectionProvider, WalletProvider as SolanaWalletProvider } from '@solana/wallet-adapter-react';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom';
+import { SolflareWalletAdapter } from '@solana/wallet-adapter-solflare';
+import { CoinbaseWalletAdapter } from '@solana/wallet-adapter-coinbase';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+
 import { clusterApiUrl } from '@solana/web3.js';
 
 export const WalletProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const network = WalletAdapterNetwork.Devnet;
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
-  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new SolflareWalletAdapter(),
+      new CoinbaseWalletAdapter(),
+    ],
+    []
+  );
 
   return (
     <ConnectionProvider endpoint={endpoint}>
